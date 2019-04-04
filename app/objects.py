993f9e7f4bf717bc887b70cd8fe9b374abf8a2f6
use-to <use-to@outlook.de>
2019-04-04 12:37:32 +0200

    fixed storage location bug

--- app/objects.py
+++ app/objects.py
@@ -2,10 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import os
 from config import config
 
-uri: str = 'sqlite:///' + config["STORAGE_LOCATION"] + "service.db"
+directory = config["STORAGE_LOCATION"]
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+uri: str = 'sqlite:///' + directory + "service.db"
 
 # uri : str = 'mysql://user:password@localhost/database'
 
