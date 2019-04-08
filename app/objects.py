06d3bf0f4cbc11bded04d107f652f9a4df076442
dre <espriworkemail@gmail.com>
2019-04-08 15:41:11 +0200

    mysql support

--- app/objects.py
+++ app/objects.py
@@ -4,17 +4,29 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from config import config
+import argparse
 
-directory = config["STORAGE_LOCATION"]
+parser: argparse.ArgumentParser = argparse.ArgumentParser()
 
-if not os.path.exists(directory):
-    os.makedirs(directory)
+parser.add_argument('--debug', help='run this service with sqlite instead of mysql only for use in develop environment',
+                    default=False, action='store_true')
 
-uri: str = 'sqlite:///' + directory + "service.db"
+args: argparse.Namespace = parser.parse_args()
 
-# uri : str = 'mysql://user:password@localhost/database'
+if args.debug is True:
 
-engine: Engine = create_engine(uri)
+    directory = config["STORAGE_LOCATION"]
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    uri: str = 'sqlite:///' + directory + "service.db"
+
+    engine: Engine = create_engine(uri)
+
+else:
+
+    engine: Engine = create_engine(config["SQLALCHEMY_DATABASE_URI"])
 
 Session = sessionmaker(bind=engine)
 
