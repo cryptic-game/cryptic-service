8de2bee81a075a19a89eb81d040b3c9b0c14b423
TheCataliasTNT2k <dubistdu2000@gmx.de>
2019-10-13 19:59:53 +0200

    added main.py (#58)

--- app/app.py
+++ app/app.py
@@ -5,12 +5,3 @@ config: Config = get_config()  # / production
 m: MicroService = MicroService("service")
 
 wrapper: DatabaseWrapper = m.get_wrapper()
-
-if __name__ == "__main__":
-    # noinspection PyUnresolvedReferences
-    from resources.service import *
-    from resources.miner import *
-    from resources.bruteforce import *
-
-    wrapper.Base.metadata.create_all(bind=wrapper.engine)
-    m.run()
