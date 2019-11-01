096a6748638362382bc312711a48de95760e3e2f
Defelo <elodef42@gmail.com>
2019-11-01 20:35:11 +0100

    Revert "Improved startup structure"
    This reverts commit a6258bbe

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
-from cryptic import MicroService, DatabaseWrapper
+from cryptic import MicroService, Config, DatabaseWrapper
 
 m: MicroService = MicroService("service")
 
 wrapper: DatabaseWrapper = m.get_wrapper()
-wrapper.Base.metadata.create_all(bind=wrapper.engine)
--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 import app
 
 
-# noinspection PyUnresolvedReferences
-def load_endpoints():
-    import resources.service
-    import resources.miner
-    import resources.bruteforce
-
-
 if __name__ == "__main__":
-    load_endpoints()
+
+    # noinspection PyUnresolvedReferences
+    from resources.service import *
+    from resources.miner import *
+    from resources.bruteforce import *
+
+    app.wrapper.Base.metadata.create_all(bind=wrapper.engine)
     app.m.run()
